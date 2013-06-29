b603899d7537f096be2296d92de3bc12ed96709e
Sam Bailey <sbailey@entropymedia.com>
2013-06-29 14:12:03 -0400

    correcting issue with animation speeds
    man that was annoying - animations were stacking up and slowing the
    scrubber

--- legacy/js/core.js
+++ legacy/js/core.js
@@ -2,6 +2,7 @@ var curvid = 0;
 var curel;
 var clipdata = new Array();
 var clipstarts = new Object();
+var clipends = new Object();
 var cliplinks = new Object();
 var cliplengths = new Object();
 var connsloaded = false;
@@ -72,8 +73,12 @@ $(document).ready(function () {
 			var clipstart_b = new Object();
 			var clipstart_c = new Object();
 			var clipstart_d = new Object();
+			var clipend_a = new Object();
+			var clipend_b = new Object();
+			var clipend_c = new Object();
+			var clipend_d = new Object();
 			for(var x = 0; x < clipdata.length; x++){
-				var cl = cliplengths[parseInt(clipdata[x].clip)];
+				var cl = Math.floor(cliplengths[parseInt(clipdata[x].clip)]);
 				if(clipdata[x].clip == "0"){
 					clipstart_a[timetosecs(clipdata[x].start)] = clipdata[x].state;
 				}
@@ -86,7 +91,19 @@ $(document).ready(function () {
 				if(clipdata[x].clip == "3"){
 					clipstart_d[timetosecs(clipdata[x].start)] = clipdata[x].state;
 				}
-				if(timetosecs(clipdata[x].start) < cl){
+				if(clipdata[x].clip == "0"){
+					clipend_a[timetosecs(clipdata[x].end)] = clipdata[x].state;
+				}
+				if(clipdata[x].clip == "1"){
+					clipend_b[timetosecs(clipdata[x].end)] = clipdata[x].state;
+				}
+				if(clipdata[x].clip == "2"){
+					clipend_c[timetosecs(clipdata[x].end)] = clipdata[x].state;
+				}
+				if(clipdata[x].clip == "3"){
+					clipend_d[timetosecs(clipdata[x].end)] = clipdata[x].state;
+				}
+				if(timetosecs(clipdata[x].end) < cl){
 					var vert = false;
 					var scr = ($("#scr_" + clipdata[x].clip));
 					// only do things within the clips we have
@@ -95,15 +112,14 @@ $(document).ready(function () {
 						vert = true;
 						pxmultiplier = Math.floor(scr.height() / cl);
 					}
-					var clipstr = '<div class="hotpoint" id="pt' + clipdata[x].clip + '_' + timetosecs(clipdata[x].start) + '" style="display: none; ';
-					var le = timetosecs(clipdata[x].end) - timetosecs(clipdata[x].start);
+					var clipstr = '<div class="hotpoint pt_' + clipdata[x].state + '" id="pt' + clipdata[x].clip + '_' + timetosecs(clipdata[x].start) + '" style="display: none; ';
+					var le = cl / (timetosecs(clipdata[x].end) - timetosecs(clipdata[x].start));
 					if(vert){
-						clipstr += 'height: ' + Math.floor(pxmultiplier * le) + 'px; margin-top: ' + Math.floor(pxmultiplier * timetosecs(clipdata[x].start)) + ' px';
+						clipstr += 'height: ' + Math.floor(pxmultiplier * le) + 'px; margin-top: ' + Math.ceil(pxmultiplier * timetosecs(clipdata[x].start)) + ' px';
 					} else {
-						clipstr += 'width: ' + Math.floor(pxmultiplier * le) + 'px; margin-left: ' + Math.floor(pxmultiplier * timetosecs(clipdata[x].start)) + ' px';
+						clipstr += 'width: ' + Math.floor(pxmultiplier * le) + 'px; margin-left: ' + Math.ceil(pxmultiplier * timetosecs(clipdata[x].start)) + ' px';
 					}
 					clipstr += '"></div>';
-					console.log(clipstr);
 					$(clipstr).appendTo(scr);
 					
 				}
@@ -114,6 +130,11 @@ $(document).ready(function () {
 			clipstarts[1] = clipstart_b;
 			clipstarts[2] = clipstart_c;
 			clipstarts[3] = clipstart_d;
+			clipends[0] = clipend_a;
+			clipends[1] = clipend_b;
+			clipends[2] = clipend_c;
+			clipends[3] = clipend_d;
+			console.log(clipends);
 		}
 	},500);
 
@@ -125,9 +146,16 @@ function progress_start (vi) {
 		// update the scrubber
 		var prg = (curel.currentTime / curel.duration) * 100;
 		if($("#scr_" + curvid + "_play").attr('data-vertical') == 1){
-			$("#scr_" + curvid + "_play").animate({ 'height': prg + '%' });
+			$("#scr_" + curvid + "_play").css({ 'height': prg + '%' });
 		} else {
-			$("#scr_" + curvid + "_play").animate({ 'width': prg + '%' });
+			$("#scr_" + curvid + "_play").css({ 'width': prg + '%' });
+		}
+		var thistime = Math.floor(curel.currentTime);
+		if(clipstarts[curvid][thistime]){
+			$(".pt_" + clipstarts[curvid][thistime]).addClass('hotpoint_on');
+		}
+		if(clipends[curvid][thistime]){
+			$(".hotpoint_on").removeClass('hotpoint_on');
 		}
 	});
 }
